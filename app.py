84e97236868a64a7a042ca84fa8709c2254a7989
MustafaPitt <mua31@pitt.edu>
2019-02-07 18:29:51 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+#
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World! from function hello_world()'
+#
+
+if __name__ == '__main__':
+    app.run()
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
