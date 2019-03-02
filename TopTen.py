df7acfb8569d07d96fd1773e2d75c15375d9063e
MustafaPitt <mua31@pitt.edu>
2019-03-01 21:00:05 -0500

    Now, the TopTen.py has 2 methods. movies() it will return the top ten movies and you_tube_url() it returns the top ten youtube video trailers. Please install the required library in order to run it

--- TopTen.py
+++ TopTen.py
@@ -1,18 +1,41 @@
+# sudo pip install google-api-python-client
+# sudo pip install lxml
+# sudo pip install pandas
 import pandas as pd
 
-url = 'https://www.imdb.com/chart/boxoffice/'
+try:
+    from googlesearch import search
+except ImportError:
+    print("No module named 'google' found")
+
+url_us_box_office = 'https://www.imdb.com/chart/boxoffice/'
+top_ten_movies = []
 
 
 # this function return an array of strings contains  current top ten movies in theatre. It parse the website specified
 # in the url variable above.
+
+
 def movies():
-    dfs = pd.read_html(url)
-    m = []
+    dfs = pd.read_html(url_us_box_office)
     for name in dfs[0]['Title']:
-        m.append(name)
-    return m
+        top_ten_movies.append(name)
+    return top_ten_movies
+
+
+def you_tube_top_ten_url():
+    youtube_urls = []
+    for m in top_ten_movies:
+        query = "youtube/" + m + "trailer"
+        for j in search(query, tld="co.in", num=1, stop=1, pause=2):
+            youtube_urls.append(j)
+    return youtube_urls
 
 
 if __name__ == '__main__':
     for x in movies():
         print(x)
+
+    for x in you_tube_top_ten_url():
+        print(x)
+
