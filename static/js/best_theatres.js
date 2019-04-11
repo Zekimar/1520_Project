53adeb9b8ff30c670aa99c8d5aa2936fe64fdf3c
Laurence Conway <conwayalex9@gmail.com>
2019-04-11 12:56:34 -0700

    default

--- static/js/best_theatres.js
+++ static/js/best_theatres.js
@@ -9,7 +9,7 @@ function get_highest_rated_callback(resp)
 	var theatre_name = resp.theatre_name;
 	var rating = resp.rating;
 	var location = document.getElementById("highest_rated_theatre_location");
-	location.innerHTML = theatre_name+", "+rating+" stars";
+	//location.innerHTML = theatre_name+", "+rating+" stars";
 }
 
 function get_lowest_price()
@@ -22,5 +22,5 @@ function get_lowest_price_callback(resp)
 	var theatre_name = resp.theatre_name;
 	var price = resp.price;
 	var location = document.getElementById("lowest_price_theatre_location");
-	location.innerHTML = theatre_name+", "+price+" dollars";
+	//location.innerHTML = theatre_name+", "+price+" dollars";
 }
